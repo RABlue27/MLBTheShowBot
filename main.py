85c22bb708666babc13e18ebf847a3129280efc4
Alex@gdplace.com <Alex@gdplace.com>
2021-06-25 02:46:46 -0400

    functional bot up and running

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import discord 
 
-# Test commit
-# Second test commit 
-# Test branch
+# Not gonna commit my token.
+f = open("secretstuff", "r")
+token = f.read()
+
+
+client = discord.Client()
+@client.event 
+async def on_ready():
+    print("Login success.")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(token)
